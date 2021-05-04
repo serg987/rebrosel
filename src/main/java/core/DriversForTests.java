0bfe1819a9d692800bb3224978e20743dca7cef7
Serg Kis <8805216@gmail.com>
2021-05-04 00:22:11 -0700

    Switch back to Win

--- src/main/java/core/DriversForTests.java
+++ src/main/java/core/DriversForTests.java
@@ -2,9 +2,9 @@ package core;
 
 // No other way to make it visible. // TODO DELETE for public repo
 public class DriversForTests {
-    public static final String CHROME_DRIVER_PATH = "src/main/resources/drivers/chromedriver";
-    public static final String FF_DRIVER_PATH = "src/main/resources/drivers/geckodriver";
-    public static final String EDGE_DRIVER_PATH = "src/main/resources/drivers/msedgedriver";
-    public static final String OPERA_DRIVER_PATH = "src/main/resources/drivers/operadriver";
-    //public static final String EDGE_EXEC_PATH = "C:/Windows/SystemApps/Microsoft.MicrosoftEdge_8wekyb3d8bbwe/MicrosoftEdge.exe";
+    public static final String CHROME_DRIVER_PATH = "src/main/resources/drivers/chromedriver.exe";
+    public static final String FF_DRIVER_PATH = "src/main/resources/drivers/geckodriver.exe";
+    public static final String EDGE_DRIVER_PATH = "src/main/resources/drivers/msedgedriver.exe";
+    public static final String OPERA_DRIVER_PATH = "src/main/resources/drivers/operadriver.exe";
+    public static final String EDGE_EXEC_PATH = "C:/Windows/SystemApps/Microsoft.MicrosoftEdge_8wekyb3d8bbwe/MicrosoftEdge.exe";
 }
